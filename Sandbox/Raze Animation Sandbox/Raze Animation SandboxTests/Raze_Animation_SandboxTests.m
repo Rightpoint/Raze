9e395f0d62c23fe84cf0cd1502564fb774ddc075
Rob Visentin <jvisenti@gmail.com>
2015-04-21 09:33:42 -0400

    added animation sandbox

---
+++ Sandbox/Raze Animation Sandbox/Raze Animation SandboxTests/Raze_Animation_SandboxTests.m
@@ -0,0 +1,40 @@
+//
+//  Raze_Animation_SandboxTests.m
+//  Raze Animation SandboxTests
+//
+//  Created by Rob Visentin on 4/21/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Raze_Animation_SandboxTests : XCTestCase
+
+@end
+
+@implementation Raze_Animation_SandboxTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
