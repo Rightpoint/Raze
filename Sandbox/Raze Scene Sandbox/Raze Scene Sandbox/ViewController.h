1dc351952846e853d2b8b8e378022271d7b7bcbb
John Stricker <john.stricker@raizlabs.com>
2015-04-21 09:17:43 -0400

    sandbox for scene work

---
+++ Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Raze Scene Sandbox
+//
+//  Created by John Stricker on 4/17/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Raze Scene Sandbox
+//
+//  Created by John Stricker on 4/17/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import "ViewController.h"
+@import RazeCore;
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    RZXQuadMesh *quad = [RZXQuadMesh quad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
