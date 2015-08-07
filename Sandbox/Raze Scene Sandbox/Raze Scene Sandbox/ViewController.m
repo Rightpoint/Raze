c6ebbe43c2de132f0ef1cb9080c7144a8b52ee89
John Stricker <john.stricker@raizlabs.com>
2015-08-07 08:13:29 -0400

    separated and added screen

--- Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.m
+++ Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.m
@@ -29,35 +29,39 @@
     effect.lightPosition = GLKVector4Make(0.0f, 10.0f, 20.0f, 0.0f);
 
     RZXScene *scene = [RZXScene sceneWithEffect: effect];
+    float ratio = CGRectGetWidth(self.view.bounds) / CGRectGetHeight(self.view.bounds);
+    scene.camera = [RZXCamera cameraWithFieldOfView:GLKMathDegreesToRadians(30) aspectRatio:ratio nearClipping:0.001 farClipping:50];
+    [scene.camera.transform setTranslation:GLKVector3Make(0.0, 1.0, 3.9)];
+    [scene.camera.transform rotateXBy:-0.25];
 
-    RZXMesh *mesh = [RZXMesh meshWithName:@"retroOffice" usingCache:YES];
-    RZXStaticTexture *texture = [RZXStaticTexture textureFromFile:@"greyTexture.png" usingCache:YES];
+    self.sceneView.scene = scene;
 
-    RZXModelNode *modelNode = [RZXModelNode modelNodeWithMesh:mesh texture:texture];
-    [modelNode.transform translateZBy:-20.0f];
+    RZXMesh *officeMesh = [RZXMesh meshWithName:@"retroOffice" usingCache:YES];
+    RZXStaticTexture *officeTexture = [RZXStaticTexture textureFromFile:@"greyTexture.png" usingCache:YES];
 
+    RZXModelNode *officeNode = [RZXModelNode modelNodeWithMesh:officeMesh texture:officeTexture];
+    [scene addChild:officeNode];
+
+    RZXMesh *screenMesh = [RZXMesh meshWithName:@"officeScreen" usingCache:YES];
+    RZXStaticTexture *screenTexture = [RZXStaticTexture textureFromFile:@"rzUnicorn.png" usingCache:YES];
+    RZXModelNode *screenNode = [RZXModelNode modelNodeWithMesh:screenMesh texture:screenTexture];
+    [officeNode addChild:screenNode];
+
+
+    /*
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
     animation.byValue = [NSValue rzx_valueWithQuaternion:GLKQuaternionMakeWithAngleAndAxis(M_PI, 0.0f, 1.0f, 0.0f)];
     animation.duration = 3.0;
     animation.repeatCount = 1000;
     [modelNode addAnimation:animation forKey:@"rotation"];
-
+*/
     RZXTextNode *textNode = [RZXTextNode nodeWithText:@"This is a test"];
     textNode.font = [RZXFont systemFontOfSize:50.0f];
     textNode.textColor = [RZXColor purpleColor];
     textNode.transform.translation = GLKVector3Make(0.0f, 0.0f, 0.34f);
   //  [modelNode addChild:textNode];
 
-    [scene addChild:modelNode];
 
-    self.sceneView.scene = scene;
-    self.view.backgroundColor = [UIColor blueColor];
-}
-
-- (void)viewWillLayoutSubviews
-{
-    float ratio = CGRectGetWidth(self.view.bounds) / CGRectGetHeight(self.view.bounds);
-    self.sceneView.scene.camera = [RZXCamera cameraWithFieldOfView:GLKMathDegreesToRadians(30) aspectRatio:ratio nearClipping:0.001 farClipping:50];
 
 }
 
