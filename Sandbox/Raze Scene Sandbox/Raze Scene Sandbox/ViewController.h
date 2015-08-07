c34e52f12d25ba789e3fe7ed93cca59d40abb83e
John Stricker <john.stricker@raizlabs.com>
2015-08-07 07:41:36 -0400

    added office scene

--- Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.h
+++ Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.h
@@ -7,6 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+@import RazeCore;
+@import RazeScene;
+@import RazeEffects;
 
 @interface ViewController : UIViewController
 
--- Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.m
+++ Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, readonly) RZXSceneView *sceneView;
+
 @end
 
 @implementation ViewController
@@ -18,6 +20,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.sceneView.backgroundColor = [UIColor whiteColor];
+    self.sceneView.framesPerSecond = 60;
+    self.sceneView.multisampleLevel = 4;
+    
+    RZXADSPhongEffect *effect = [RZXADSPhongEffect effect];
+    effect.lightPosition = GLKVector4Make(0.0f, 10.0f, 20.0f, 0.0f);
+
+    RZXScene *scene = [RZXScene sceneWithEffect: effect];
+
+    RZXMesh *mesh = [RZXMesh meshWithName:@"retroOffice" usingCache:YES];
+    RZXStaticTexture *texture = [RZXStaticTexture textureFromFile:@"greyTexture.png" usingCache:YES];
+
+    RZXModelNode *modelNode = [RZXModelNode modelNodeWithMesh:mesh texture:texture];
+    [modelNode.transform translateZBy:-20.0f];
+
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    animation.byValue = [NSValue rzx_valueWithQuaternion:GLKQuaternionMakeWithAngleAndAxis(M_PI, 0.0f, 1.0f, 0.0f)];
+    animation.duration = 3.0;
+    animation.repeatCount = 1000;
+    [modelNode addAnimation:animation forKey:@"rotation"];
+
+    RZXTextNode *textNode = [RZXTextNode nodeWithText:@"This is a test"];
+    textNode.font = [RZXFont systemFontOfSize:50.0f];
+    textNode.textColor = [RZXColor purpleColor];
+    textNode.transform.translation = GLKVector3Make(0.0f, 0.0f, 0.34f);
+  //  [modelNode addChild:textNode];
+
+    [scene addChild:modelNode];
+
+    self.sceneView.scene = scene;
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (void)viewWillLayoutSubviews
+{
+    float ratio = CGRectGetWidth(self.view.bounds) / CGRectGetHeight(self.view.bounds);
+    self.sceneView.scene.camera = [RZXCamera cameraWithFieldOfView:GLKMathDegreesToRadians(30) aspectRatio:ratio nearClipping:0.001 farClipping:50];
+
+}
+
+- (RZXSceneView *)sceneView
+{
+    return (RZXSceneView *)self.view;
 }
 
 @end
