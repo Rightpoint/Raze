a544bc5878cc60161a59e60b6daeeaecaedf2f40
John Stricker <john.stricker@raizlabs.com>
2015-08-07 09:16:37 -0400

    added recognizers

--- Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.m
+++ Sandbox/Raze Scene Sandbox/Raze Scene Sandbox/ViewController.m
@@ -12,7 +12,7 @@
 @interface ViewController ()
 
 @property (nonatomic, readonly) RZXSceneView *sceneView;
-
+@property (nonatomic, strong) RZXModelNode *officeNode;
 @end
 
 @implementation ViewController
@@ -29,8 +29,9 @@
     effect.lightPosition = GLKVector4Make(0.0f, 10.0f, 20.0f, 0.0f);
 
     RZXScene *scene = [RZXScene sceneWithEffect: effect];
+
     float ratio = CGRectGetWidth(self.view.bounds) / CGRectGetHeight(self.view.bounds);
-    scene.camera = [RZXCamera cameraWithFieldOfView:GLKMathDegreesToRadians(30) aspectRatio:ratio nearClipping:0.001 farClipping:50];
+    scene.camera = [RZXCamera cameraWithFieldOfView:GLKMathDegreesToRadians(35) aspectRatio:ratio nearClipping:0.001 farClipping:50];
     [scene.camera.transform setTranslation:GLKVector3Make(0.0, 1.0, 3.9)];
     [scene.camera.transform rotateXBy:-0.25];
 
@@ -41,28 +42,56 @@
 
     RZXModelNode *officeNode = [RZXModelNode modelNodeWithMesh:officeMesh texture:officeTexture];
     [scene addChild:officeNode];
+    self.officeNode = officeNode;
 
     RZXMesh *screenMesh = [RZXMesh meshWithName:@"officeScreen" usingCache:YES];
     RZXStaticTexture *screenTexture = [RZXStaticTexture textureFromFile:@"rzUnicorn.png" usingCache:YES];
     RZXModelNode *screenNode = [RZXModelNode modelNodeWithMesh:screenMesh texture:screenTexture];
     [officeNode addChild:screenNode];
 
+    self.view.userInteractionEnabled = YES;
+    UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    [self.view addGestureRecognizer:panRecognizer];
 
-    /*
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+    [self.view addGestureRecognizer:tapRecognizer];
+
+    UIPinchGestureRecognizer *pinchRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinch:)];
+    [self.view addGestureRecognizer:pinchRecognizer];
+
+    UILongPressGestureRecognizer *longPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
+    [self.view addGestureRecognizer:longPressRecognizer];
+}
+
+- (void)handlePan:(UIPanGestureRecognizer *)panRecognizer
+{
+    [self.officeNode.transform rotateYBy: [panRecognizer velocityInView:self.view].x * -0.001];
+}
+
+- (void)handleTap:(UITapGestureRecognizer *)tapRecognizer
+{
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
-    animation.byValue = [NSValue rzx_valueWithQuaternion:GLKQuaternionMakeWithAngleAndAxis(M_PI, 0.0f, 1.0f, 0.0f)];
-    animation.duration = 3.0;
-    animation.repeatCount = 1000;
-    [modelNode addAnimation:animation forKey:@"rotation"];
-*/
-    RZXTextNode *textNode = [RZXTextNode nodeWithText:@"This is a test"];
-    textNode.font = [RZXFont systemFontOfSize:50.0f];
-    textNode.textColor = [RZXColor purpleColor];
-    textNode.transform.translation = GLKVector3Make(0.0f, 0.0f, 0.34f);
-  //  [modelNode addChild:textNode];
+    animation.toValue = [NSValue rzx_valueWithQuaternion:GLKQuaternionMakeWithAngleAndAxis(0.0, 0.0, 0.0, 0.0)];
+    animation.duration = 0.2;
+    [self.officeNode addAnimation:animation forKey:@"rotation"];
 
+    CABasicAnimation *translationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.translation"];
+    animation.toValue = [NSValue rzx_valueWithVec3:GLKVector3Make(0.0, 0.01, 0.0)];
+    animation.duration = 0.2;
+    [self.officeNode addAnimation:translationAnimation forKey:@"translation"];
+}
 
+- (void)handlePinch:(UIPinchGestureRecognizer *)pinchRecognizer
+{
+    [self.officeNode.transform translateZBy:pinchRecognizer.velocity * 0.01];
+}
 
+- (void)handleLongPress:(UILongPressGestureRecognizer *)longPressRecognizer
+{
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.translation"];
+    animation.toValue = [NSValue rzx_valueWithVec3:GLKVector3Make(0.0, 1.0, 3.0)];
+    animation.duration = 0.4;
+    [self.officeNode addAnimation:animation forKey:@"translation"];
 }
 
 - (RZXSceneView *)sceneView
