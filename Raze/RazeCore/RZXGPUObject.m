ac910c7f584e0cbee68e86e3dd92e7c9d0eb94c2
Rob Visentin <jvisenti@gmail.com>
2016-06-21 12:10:48 -0400

    better support for multiple contexts

--- Raze/RazeCore/RZXGPUObject.m
+++ Raze/RazeCore/RZXGPUObject.m
@@ -23,14 +23,16 @@
 
 - (BOOL)setupGL
 {
-    // TODO: when supporting multiple contexts, also ensure that the current context is the configured context
+    RZXGLContext *currentContext = [RZXGLContext currentContext];
+
     BOOL setup = (self.configuredContext != nil);
 
+    if ( setup && currentContext != nil && currentContext != self.configuredContext ) {
+        [self teardownGL];
+        setup = NO;
+    }
+
     if ( !setup ) {
-        // TODO: when supporting multiple contexts, also teardown in previous context
-
-        RZXGLContext *currentContext = [RZXGLContext currentContext];
-
         if ( currentContext != nil ) {
             self.configuredContext = currentContext;
             setup = YES;
@@ -45,8 +47,14 @@
 
 - (BOOL)bindGL
 {
-    // TODO: when supporting multiple contexts, also ensure that the current context is the configured context
+    RZXGLContext *currentContext = [RZXGLContext currentContext];
+
     BOOL bound = (self.configuredContext != nil);
+
+    if ( bound && currentContext != nil && currentContext != self.configuredContext ) {
+        [self teardownGL];
+        bound = NO;
+    }
 
     if ( !bound ) {
         bound = [self setupGL];
